33f686c3523e2c16955565a0ccfd995d54767b70
Karthik N <karthikn@thoughtworks.com>
2019-12-07 09:20:42 +0530

    Spring cloud bus with amqp

--- spring-cloud-config-server/src/test/java/com/kk/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
+++ spring-cloud-config-server/src/test/java/com/kk/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.kk.microservices.springcloudconfigserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
