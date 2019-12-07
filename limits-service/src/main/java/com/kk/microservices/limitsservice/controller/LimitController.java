21beabeadc4f33905eb34eb469a9bc64def4b8b9
Karthik N <karthikn@thoughtworks.com>
2019-12-07 13:49:21 +0530

    Added Hystrix for folt tollerent and discover config server via naming server

--- limits-service/src/main/java/com/kk/microservices/limitsservice/controller/LimitController.java
+++ limits-service/src/main/java/com/kk/microservices/limitsservice/controller/LimitController.java
@@ -2,6 +2,7 @@ package com.kk.microservices.limitsservice.controller;
 
 import com.kk.microservices.limitsservice.Configuration;
 import com.kk.microservices.limitsservice.model.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +15,17 @@ public class LimitController {
     @GetMapping(path = "/limits")
     public LimitConfiguration getLimits(){
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+
+    @GetMapping(path = "/limits-with-hystrix")
+    @HystrixCommand(fallbackMethod = "getLimitsDefault")
+    public LimitConfiguration getLimitsFails(){
+
+        throw  new RuntimeException("");
+    }
+
+    public LimitConfiguration getLimitsDefault(){
+        return new LimitConfiguration(100, 0);
     }
 }
